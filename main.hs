dbc2a7bb675799468051073fe2cf8a9df2c88930
ericlovesmath <ericlovesmath@gmail.com>
2024-11-16 12:13:12 -0800

    Major Chord / Beat Frequency / Overtone Examples

--- main.hs
+++ main.hs
@@ -55,10 +55,32 @@ triangle = mkWave (\t -> 4 * abs (t - down (t + 3 / 4) + 1 / 4) - 1)
 saw :: Seconds -> Hz -> Wave
 saw = mkWave (\t -> 2 * (t - down (t + 1 / 2)))
 
+major :: Seconds -> Wave
+major time =
+    attack 0.0001
+    $ map sum . transpose
+    $ map (sine time . semitoneToHz) [0, 4, 7]
+  where
+    semitoneToHz n = 440.0 * (2 ** (1 / 12)) ** n
+    attack rate = zipWith (*) (map (min 1.0) [0.0, rate ..])
+
+beat :: Seconds -> Wave
+beat time = zipWith (+) (sine time 440) (sine time 442)
+
+overtones :: Wave
+overtones =
+    map sum . transpose
+    $ zipWith (map . (*)) volumes
+    $ map (sine 3) freqs
+  where
+    count = 6
+    volumes = [1 - i / count | i <- [0 ..]]
+    freqs = map (110 *) [1 .. count]
+
 main :: IO ()
 main = play $ clamped $ muted $ intercalate silence waves
   where
-    waves = [sine 1 440, square 1 440, triangle 1 440, saw 1 440]
+    waves = [sine 1 440, square 1 440, triangle 1 440, saw 1 440, major 2, beat 2, overtones]
     silence = sine 0.2 0
     muted = map (* 0.3)
     clamped = map (min 1.0 . max (-1.0))
