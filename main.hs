eafaeba45b0279b1233015af3bbba93647939b73
ericlovesmath <ericlovesmath@gmail.com>
2024-11-16 12:39:07 -0800

    Conversion to Lazy Signals

--- main.hs
+++ main.hs
@@ -6,18 +6,23 @@ import Data.List
 import System.IO
 import System.Process
 
-type Wave = [Float]
 type Seconds = Float
+type Pulse = Float
 type Hz = Float
 
 sampleRate :: Hz
 sampleRate = 48000.0
 
-save :: FilePath -> Wave -> IO ()
-save path = BL.writeFile path . toLazyByteString . foldMap floatLE
+newtype Signal = Sig (Seconds -> Pulse)
 
-play :: Wave -> IO ()
-play wave = do
+signalToBytes :: Signal -> BL.ByteString
+signalToBytes (Sig f) = toLazyByteString $ foldMap (floatLE . f) [0.0, 1 / sampleRate ..]
+
+save :: FilePath -> Seconds -> Signal -> IO ()
+save path time = BL.writeFile path . BL.take (round (sampleRate * time)) . signalToBytes
+
+play :: Signal -> IO ()
+play signal = do
     let args =
             [ "-hide_banner"
             , "-loglevel" , "error"
@@ -28,59 +33,56 @@ play wave = do
             , "pipe:0"
             ]
     (Just stdin, _, _, p) <- createProcess (proc "ffplay" args) {std_in = CreatePipe}
-    BL.hPut stdin $ toLazyByteString (foldMap floatLE wave)
+    BL.hPut stdin $ signalToBytes signal
     hFlush stdin
     hClose stdin
     waitForProcess p
     return ()
 
-timestamps :: Seconds -> [Seconds]
-timestamps time = [0.0, 1 / sampleRate .. time]
-
-mkWave :: (Seconds -> Float) -> Seconds -> Hz -> Wave
-mkWave f time hz = map (f . (hz *)) $ timestamps time
-
 down :: Float -> Float
 down x = fromIntegral (floor x :: Integer)
 
-sine :: Seconds -> Hz -> Wave
-sine = mkWave (\t -> sin (2 * pi * t))
+sine :: Hz -> Signal
+sine hz = Sig (\t -> sin (2 * pi * t * hz))
 
-square :: Seconds -> Hz -> Wave
-square = mkWave (\t -> 4 * down t - 2 * down (2 * t) + 1)
+square :: Hz -> Signal
+square hz = Sig (\t -> 4 * down (t * hz) - 2 * down (2 * t * hz) + 1)
 
-triangle :: Seconds -> Hz -> Wave
-triangle = mkWave (\t -> 4 * abs (t - down (t + 3 / 4) + 1 / 4) - 1)
+triangle :: Hz -> Signal
+triangle hz = Sig (\t -> 4 * abs (t * hz - down (t * hz + 3 / 4) + 1 / 4) - 1)
 
-saw :: Seconds -> Hz -> Wave
-saw = mkWave (\t -> 2 * (t - down (t + 1 / 2)))
-
-major :: Seconds -> Wave
-major time =
-    attack 0.0001
-    $ map sum . transpose
-    $ map (sine time . semitoneToHz) [0, 4, 7]
-  where
-    semitoneToHz n = 440.0 * (2 ** (1 / 12)) ** n
-    attack rate = zipWith (*) (map (min 1.0) [0.0, rate ..])
-
-beat :: Seconds -> Wave
-beat time = zipWith (+) (sine time 440) (sine time 442)
-
-overtones :: Wave
-overtones =
-    map sum . transpose
-    $ zipWith (map . (*)) volumes
-    $ map (sine 3) freqs
-  where
-    count = 6
-    volumes = [1 - i / count | i <- [0 ..]]
-    freqs = map (110 *) [1 .. count]
+saw :: Hz -> Signal
+saw hz = Sig (\t -> 2 * (t * hz - down (t * hz + 1 / 2)))
 
 main :: IO ()
-main = play $ clamped $ muted $ intercalate silence waves
-  where
-    waves = [sine 1 440, square 1 440, triangle 1 440, saw 1 440, major 2, beat 2, overtones]
-    silence = sine 0.2 0
-    muted = map (* 0.3)
-    clamped = map (min 1.0 . max (-1.0))
+main = play (saw 440)
+
+-- major :: Seconds -> Wave
+-- major time =
+--     attack 0.0001
+--     $ map sum . transpose
+--     $ map (sine time . semitoneToHz) [0, 4, 7]
+--   where
+--     semitoneToHz n = 440.0 * (2 ** (1 / 12)) ** n
+--     attack rate = zipWith (*) (map (min 1.0) [0.0, rate ..])
+--
+-- beat :: Seconds -> Wave
+-- beat time = zipWith (+) (sine time 440) (sine time 442)
+--
+-- overtones :: Wave
+-- overtones =
+--     map sum . transpose
+--     $ zipWith (map . (*)) volumes
+--     $ map (sine 3) freqs
+--   where
+--     count = 6
+--     volumes = [1 - i / count | i <- [0 ..]]
+--     freqs = map (110 *) [1 .. count]
+--
+-- main :: IO ()
+-- main = play $ clamped $ muted $ intercalate silence waves
+--   where
+--     waves = [sine 1 440, square 1 440, triangle 1 440, saw 1 440, major 2, beat 2, overtones]
+--     silence = sine 0.2 0
+--     muted = map (* 0.3)
+--     clamped = map (min 1.0 . max (-1.0))
