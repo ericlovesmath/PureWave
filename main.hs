62bb946bc08c18510f0e3b4c022617691bc73c35
ericlovesmath <ericlovesmath@gmail.com>
2024-11-16 14:21:07 -0800

    Num and Functor based Signals

--- main.hs
+++ main.hs
@@ -13,15 +13,27 @@ type Hz = Float
 sampleRate :: Hz
 sampleRate = 48000.0
 
-newtype Signal = Sig (Seconds -> Pulse)
+newtype Signal a = Sig (Seconds -> a)
+type Wave = Signal Pulse
 
-signalToBytes :: Signal -> BL.ByteString
+instance Num a => Num (Signal a) where
+    Sig f + Sig g = Sig (\t -> f t + g t)
+    Sig f - Sig g = Sig (\t -> f t - g t)
+    Sig f * Sig g = Sig (\t -> f t * g t)
+    abs (Sig f) = Sig (abs . f)
+    signum (Sig f) = Sig (signum . f)
+    fromInteger = Sig . const . fromInteger
+
+instance Functor Signal where
+    fmap f (Sig g) = Sig (fmap f g)
+
+signalToBytes :: Wave -> BL.ByteString
 signalToBytes (Sig f) = toLazyByteString $ foldMap (floatLE . f) [0.0, 1 / sampleRate ..]
 
-save :: FilePath -> Seconds -> Signal -> IO ()
+save :: FilePath -> Seconds -> Wave -> IO ()
 save path time = BL.writeFile path . BL.take (round (sampleRate * time)) . signalToBytes
 
-play :: Signal -> IO ()
+play :: Wave -> IO ()
 play signal = do
     let args =
             [ "-hide_banner"
@@ -42,43 +54,40 @@ play signal = do
 down :: Float -> Float
 down x = fromIntegral (floor x :: Integer)
 
-sine :: Hz -> Signal
+sine :: Hz -> Wave
 sine hz = Sig (\t -> sin (2 * pi * t * hz))
 
-square :: Hz -> Signal
+square :: Hz -> Wave
 square hz = Sig (\t -> 4 * down (t * hz) - 2 * down (2 * t * hz) + 1)
 
-triangle :: Hz -> Signal
+triangle :: Hz -> Wave
 triangle hz = Sig (\t -> 4 * abs (t * hz - down (t * hz + 3 / 4) + 1 / 4) - 1)
 
-saw :: Hz -> Signal
+saw :: Hz -> Wave
 saw hz = Sig (\t -> 2 * (t * hz - down (t * hz + 1 / 2)))
 
-main :: IO ()
-main = play (saw 440)
+major :: Wave
+major = fmap (* 0.3) $ sum $ map (sine . semitoneToHz) [0, 4, 7]
+  where
+    semitoneToHz n = 440.0 * (2 ** (1 / 12)) ** n
+    -- attack rate = zipWith (*) (map (min 1.0) [0.0, rate ..])
 
--- major :: Seconds -> Wave
--- major time =
---     attack 0.0001
---     $ map sum . transpose
---     $ map (sine time . semitoneToHz) [0, 4, 7]
---   where
---     semitoneToHz n = 440.0 * (2 ** (1 / 12)) ** n
---     attack rate = zipWith (*) (map (min 1.0) [0.0, rate ..])
---
--- beat :: Seconds -> Wave
--- beat time = zipWith (+) (sine time 440) (sine time 442)
---
--- overtones :: Wave
--- overtones =
---     map sum . transpose
---     $ zipWith (map . (*)) volumes
---     $ map (sine 3) freqs
---   where
---     count = 6
---     volumes = [1 - i / count | i <- [0 ..]]
---     freqs = map (110 *) [1 .. count]
---
+beat :: Wave
+beat = (* 0.5) <$> sine 440 + sine 442
+
+overtones :: Wave
+overtones =
+    fmap (* 0.5)
+    $ sum
+    $ zipWith (fmap . (*)) volumes
+    $ map (sine . (110 *)) [1 .. count]
+  where
+    count = 6
+    volumes = [1 - i / count | i <- [0 ..]]
+
+main :: IO ()
+main = play overtones
+
 -- main :: IO ()
 -- main = play $ clamped $ muted $ intercalate silence waves
 --   where
